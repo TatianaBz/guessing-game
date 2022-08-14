56f71c3e85f3f9dd110819d10c3d5912c3526744
tatiana <name>
2022-08-14 14:19:24 +0700

    GuessingGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,23 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() { }
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.current = Math.round((this.min + this.max)/2);
+        return this.current;
     }
 
     lower() {
-
+        this.max = this.current;
     }
 
     greater() {
-
+        this.min = this.current;
     }
 }
 
 module.exports = GuessingGame;
+
